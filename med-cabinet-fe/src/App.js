2a4d8b16c0d583323f36ca2422fd7840b1b88fdf
richknicks <rlynch@gmail.com>
2020-02-04 15:04:13 -0500

    fixed app.js

--- med-cabinet-fe/src/App.js
+++ med-cabinet-fe/src/App.js
@@ -5,18 +5,10 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
+
     <LandingPage />
   </div>
 )
   }
 
 export default App
-=======
-      <LandingPage />
-    </div>
-  );
-}
-
-export default App;
->>>>>>> 9814062ab91ca8412b7f38306259c480afb4b538
